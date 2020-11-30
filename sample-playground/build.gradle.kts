d09f2b2d9a3df91ff1e33e47f17c5c26f39e6729
iroyo <iroyo.dev@protonmail.com>
2020-11-30 17:22:35 +0100

    including webextension-polyfill to output

--- sample-playground/build.gradle.kts
+++ sample-playground/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":weked-firefox"))
     implementation(npm("webextension-polyfill", "0.7.0"))
+    implementation(project(":weked-firefox"))
     implementation(kotlin("stdlib-js"))
     implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
 }
@@ -18,6 +18,12 @@ kotlin {
         browser {
             distribution {
                 directory = File("$projectDir/extension/output/")
+                copy {
+                    from("$rootDir/build/js/node_modules/webextension-polyfill/dist") {
+                        include("browser-polyfill.min.js")
+                    }
+                    into("$projectDir/extension/output/")
+                }
             }
             webpackTask {
                 cssSupport.enabled = true
